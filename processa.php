a2592416a97e9974f6221abfe7b2feade388d540
Leonardo Akahoshi <41695114+Akahoshi21@users.noreply.github.com>
2020-11-26 13:46:30 -0300

    Update processa.php

--- processa.php
+++ processa.php
@@ -2,18 +2,19 @@
 session_start();
 include_once("conexao.php");
 
-if(empty($_POST['autor']) || empty($_POST['titulo']) || empty($_POST['editora']) || empty($_POST['imgurl'])){
+if(empty($_POST['autor']) || empty($_POST['titulo']) || empty($_POST['user']) || empty($_POST['editora']) || empty($_POST['imgurl'])){
 	$_SESSION['msg'] = "<p style='color:red;'>Livro não foi cadastrado com sucesso</p>";
 	header('Location: Cadlivros.php');
 exit();
 }
 
+$user = filter_input(INPUT_POST, 'user', FILTER_SANITIZE_STRING);
 $autor = filter_input(INPUT_POST, 'autor', FILTER_SANITIZE_STRING);
 $titulo = filter_input(INPUT_POST, 'titulo', FILTER_SANITIZE_STRING);
 $editora = filter_input(INPUT_POST, 'editora', FILTER_SANITIZE_STRING);
 $imgurl = filter_input(INPUT_POST, 'imgurl', FILTER_SANITIZE_STRING);
 
-$result_usuario = "INSERT INTO livrosc (autor, titulo, editora, imgurl) VALUES ('$autor', '$titulo', '$editora','$imgurl' )";
+$result_usuario = "INSERT INTO livrosc (autor, titulo, editora, imgurl) VALUES ('$autor', '$titulo','$user', '$editora','$imgurl' )";
 $resultado_usuario = mysqli_query($conexao, $result_usuario);
 
 if(mysqli_insert_id($conexao)){
